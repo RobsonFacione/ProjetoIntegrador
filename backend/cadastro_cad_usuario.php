d211f2e0352c4075ec92fbf41f586a47f2a64494
Robson Facione <rolufa30@gmail.com>
2023-10-02 20:15:32 -0300

    tela de login e cadastro de usuario atualizadas

--- backend/cadastro_cad_usuario.php
+++ backend/cadastro_cad_usuario.php
@@ -1,1 +1,23 @@
-<!-- fazer o código para preencher a tabela do banco de dados
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $cpf = $_POST["cpf"];
+        $senha = $_POST["senha"];
+
+        $conexao = new mysqli("localhost", "root", "", "agendamento");
+        if ($conexao->connect_error) {
+            die("Conexão falhou: " . $conexao->connect_error);
+        }
+
+        $sql = "INSERT INTO cad_usuario (nome, email, cpf, senha) VALUES ('$nome', '$email','$cpf',  '$senha')";
+        if ($conexao->query($sql) === TRUE) {
+            echo '<script>alert("Registro cadastrado com sucesso.");</script>';
+            echo '<meta http-equiv="refresh" content="2;url=menu.html">';
+        } else {
+            echo "Erro ao cadastrar registro: " . $conexao->error;
+        }
+
+        $conexao->close();
+    }
+?>
