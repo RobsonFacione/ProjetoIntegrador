a505afbfd08c222f8b7899aba7f328c6b184d626
Robson Facione <rolufa30@gmail.com>
2023-10-29 14:34:50 -0300

    editar agendamento

---
+++ backend/excluir_agendamento.php
@@ -0,0 +1,30 @@
+<?php
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    // Conexão com o banco de dados
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "agendamento";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
+    // Consulta SQL para excluir o agendamento
+    $sql = "DELETE FROM vw_agendamento WHERE ID = $id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Agendamento excluído com sucesso.";
+    } else {
+        echo "Erro ao excluir o agendamento: " . $conn->error;
+    }
+
+    $conn->close();
+} else {
+    echo "ID do agendamento não especificado.";
+}
+
+?>
